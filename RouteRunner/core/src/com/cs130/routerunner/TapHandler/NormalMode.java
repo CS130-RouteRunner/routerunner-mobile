f71cee81ea7c895c47aa5ec3e423e6681dde0a42
Julian Yang <yang.julian@gmail.com>
2015-10-23 15:27:04 -0700

    do some refactor/renaming

--- RouteRunner/core/src/com/cs130/routerunner/TapHandler/NormalMode.java
+++ RouteRunner/core/src/com/cs130/routerunner/TapHandler/NormalMode.java
@@ -4,6 +4,11 @@ package com.cs130.routerunner.TapHandler;
  * Created by julianyang on 10/22/15.
  */
 public class NormalMode implements TapMode {
+    TapHandler tapHandler_;
+
+    public NormalMode(TapHandler tapHandler) {
+        this.tapHandler_ = tapHandler;
+    }
     public void Tap(float x, float y, int count) {
         // check if the tap collides with any buttons
         // check if the tap collides with any actors
--- RouteRunner/core/src/com/cs130/routerunner/TapHandler/RouteEditMode.java
+++ RouteRunner/core/src/com/cs130/routerunner/TapHandler/RouteEditMode.java
@@ -6,7 +6,12 @@ import com.cs130.routerunner.Actor;
  * Created by julianyang on 10/22/15.
  */
 public class RouteEditMode implements TapMode {
-    Actor selectedActor;
+    Actor selectedActor_;
+    TapHandler tapHandler_;
+    public RouteEditMode(TapHandler tapHandler) {
+       this.tapHandler_ = tapHandler;
+    }
+
     public void Tap(float x, float y, int count) {
         // we have entered setting a waypoint mode, so push this waypoint to
         // the actor.
