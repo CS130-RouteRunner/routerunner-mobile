5163051488f462ca85fbeae045c68ee5dd4abffc
Julian Yang <yang.julian@gmail.com>
2015-10-26 14:42:32 -0700

    add comments on how to implement states for TapHandler

--- RouteRunner/core/src/com/cs130/routerunner/TapHandler/NormalMode.java
+++ RouteRunner/core/src/com/cs130/routerunner/TapHandler/NormalMode.java
@@ -12,6 +12,11 @@ public class NormalMode implements TapMode {
     public void Tap(float x, float y, int count) {
         // check if the tap collides with any buttons
         // check if the tap collides with any actors
-        // else do nothing
+        // TODO(Kailin): if tap collides with the truck, switch to
+        // SelectedActorMode (note that we also need to implement that too)
+        // 1) SelectedActor should change state to RouteEditMode when the user
+        // clicks on SelectedActorMode
+        // 2) Before changing to SelectedActor, we need to display an
+        // EditRoute Button.
     }
 }
