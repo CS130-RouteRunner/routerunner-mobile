00e13b243c9fa88cdfc02100947ac14f256c4246
Roger <rlau4545@gmail.com>
2015-10-25 18:05:30 -0700

    refactored routes into its own folder

---
+++ RouteRunner/core/src/com/cs130/routerunner/Routes/RouteFactory.java
@@ -0,0 +1,28 @@
+package com.cs130.routerunner.Routes;
+
+import com.badlogic.gdx.math.Vector3;
+import com.cs130.routerunner.Actor;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Roger on 10/23/2015.
+ */
+public class RouteFactory {
+    public ArrayList<Vector3> wayPoints;
+
+    public void startCreatingRoute(){
+        wayPoints = new ArrayList<Vector3>();
+    }
+
+    public void addWayPoint(float x, float y){
+        Vector3 v = new Vector3(x, y, 0f);
+        wayPoints.add(v);
+    }
+
+    public Route getRoute(Actor truck){
+        Route r = new Route(truck);
+        r.setWayPoints(wayPoints);
+        return r;
+    }
+}
