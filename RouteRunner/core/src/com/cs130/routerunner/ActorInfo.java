6119c60774d09986ce3a4ec7d57f4b756b46bae9
Evan <gezhiliu942015@gmail.com>
2015-10-29 03:19:30 -0700

    change dialog size

--- RouteRunner/core/src/com/cs130/routerunner/ActorInfo.java
+++ RouteRunner/core/src/com/cs130/routerunner/ActorInfo.java
@@ -2,6 +2,7 @@ package com.cs130.routerunner;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -19,12 +20,18 @@ public class ActorInfo {
     private Skin skin_;
     private boolean editRoute_;
     private InputProcessor preProcessor_;
+    private TextureAtlas atlas_;
+    private final float dialogWidth_ = 800f;
+    private final float dialogHeight_ = 450f;
+    private final float buttonWidth_ = 380f;
+    private final float buttonHeight_ = 100f;
 
     public ActorInfo(Stage stage){
         stage_ = stage;
         editRoute_ = false;
     }
 
+    // below two functions may refactor with InputMultiplexer
     protected void updateInputProcessor(InputProcessor inputProcessor){
         preProcessor_ = Gdx.input.getInputProcessor();
         Gdx.input.setInputProcessor(inputProcessor);
@@ -37,16 +44,32 @@ public class ActorInfo {
 
     public void display(){
         updateInputProcessor(stage_);
+        //atlas_ = new TextureAtlas(Gdx.files.internal("ui-blue.atlas"));
         skin_ = new Skin(Gdx.files.internal("uiskin.json"));
-        dialog_ = new Dialog("Info", skin_, "dialog");
-        Button yesButton = new TextButton("yes", skin_);
+        dialog_ = new Dialog("Info", skin_, "dialog"){
+            @Override
+            public float getPrefHeight() { return dialogHeight_; }
+            @Override
+            public float getPrefWidth() { return dialogWidth_; }
+        };
+        Button yesButton = new TextButton("yes", skin_){
+            @Override
+            public float getPrefWidth() { return buttonWidth_; }
+            @Override
+            public float getPrefHeight() { return buttonHeight_; }
+        };
         yesButton.addListener(new ClickListener() {
             public void clicked(InputEvent event, float x, float y) {
                 editRoute_ = true;
                 hide();
             }
         });
-        Button noButton = new TextButton("no", skin_);
+        Button noButton = new TextButton("no", skin_){
+            @Override
+            public float getPrefWidth() { return buttonWidth_; }
+            @Override
+            public float getPrefHeight() { return buttonHeight_; }
+        };
         noButton.addListener(new ClickListener(){
             public void clicked(InputEvent event, float x, float y) {
                 editRoute_ = false;
