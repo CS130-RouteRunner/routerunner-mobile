b4b2608f77c05f0c82fe8e59455fa408ea1cc1b6
Julian Yang <yang.julian@gmail.com>
2015-11-30 00:12:37 -0800

    lower win requirements and add teleporting trucks for sync

--- RouteRunner/core/src/com/cs130/routerunner/Settings.java
+++ RouteRunner/core/src/com/cs130/routerunner/Settings.java
@@ -62,8 +62,8 @@ public class Settings {
     public static final int BUY_MISSILE_COST = 200;
     public static final int TRUCK_UPGRADE_COST = 50;
     public static final int TRUCK_UPGRADE_CARRY = 180;
-    public static final int FRAMES_BETWEEN_TRY_EVENT = 1200;
-    public static final int TARGET_MONEY = 600;    // TODO: Change this after win syncing implemented
+    public static final int FRAMES_BETWEEN_TRY_EVENT = 1600;
+    public static final int TARGET_MONEY = 1000;
 
     // Random Events
     public static final String RANDOM_EVENT_PNG = "treasure.png";
